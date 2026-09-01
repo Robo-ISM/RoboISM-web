643816a0c1d2a86d7f85cca86c70a8911f42c49f
RoboISM <robotics_club@iitism.ac.in>
2026-09-01 19:52:17 +0530

    :)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,20 +25,24 @@ export default function Home() {
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             The official{" "}
             <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              href="https://roboism.vercel.app/#"
               className="font-medium text-zinc-950 dark:text-zinc-50"
             >
               Robotics
             </a>{" "}
             and{" "}
             <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              href="https://roboism.vercel.app/#"
               className="font-medium text-zinc-950 dark:text-zinc-50"
             >
               AI Club
             </a>{" "}
             of IIT (ISM) Dhanbad.
           </p>
+          <p>
+            This website is currently under construction. Please check back later for updates.
+          </p>
+          <p> – by Keshav Kundan 😎</p>
         </div>
         {/*<div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
